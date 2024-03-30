176269c0179e88b691329d6601785d2a67cca20c
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-03-31 00:57:28 +0600

    react-router-dom inalled and updated the header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
 
   return (
     <>
-      
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 className="text-4xl text-white bg-green-600 rounded-md p-3 font-mono font-semibold italic">
+        Ugyen ❤️ Decheen
+      </h1>
     </>
-  )
-}
+  );
+}s
 
-export default App
+export default App;
