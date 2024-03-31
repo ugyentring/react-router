136f44fced704906cd705ba124c24de94487eff1
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-03-31 14:28:13 +0600

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 // import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header/Header"
+import Home from "./components/Home/Home"
+import Footer from "./components/Footer/Footer"
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-4xl text-white bg-green-600 rounded-md p-3 font-mono font-semibold italic">
-        Ugyen ❤️ Decheen
-      </h1>
+      <Header/>
+      <Home/>
+      <Footer/>
     </>
   );
-}s
+}
 
 export default App;
